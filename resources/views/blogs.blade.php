d9f85d628cf7c896e45f60f563aa6d14c1e096a4
OlePundit <glennomondi@yahoo.com>
2024-06-26 04:19:34 +0300

    blogs

--- resources/views/blogs.blade.php
+++ resources/views/blogs.blade.php
@@ -2,25 +2,23 @@
 
 @section('content')
 
-        <div class="container ">
-            <div class="row justify-content-center">
+<div class="blog_section layout_padding ">
+    <h1>Blogs</h1>
+    <div class="row justify-content-center">
+    @foreach($blogs as $blog)
+    <div class="col-lg-3 mt-5">
+        <div class="card" style="width: 100%;">
+            <img src="/storage/{{$blog->thumbnail}}" class="card-img-top">
+            <div class="card-body">
+                <h4 class="card-title">{{$blog->title}}</h4>
+                <p class="card-text">{!! Str::limit(strip_tags($blog->body), 120, '...') !!}...</p>
+                <a href="/blogs/{{$blog->slug}}" style="text-decoration:none;" class="custom-card">
 
-                <div class="container" style="width:70%;margin:auto;">
-        
-                </div>
-                <div class="row mt-5 section-more">
-                    @foreach($blogs as $blog)
-                    <a href="/blogs/{{$blog->slug}}" style="text-decoration:none;" class="custom-card">
-                        <img src="/storage/{{$blog->thumbnail}}" class="card-img-top">
-                        <div class="card-body">
-                            <h4>{{$blog->title}}</h4>
-                            <p>{!! Str::limit(strip_tags($blog->body), 120, '...') !!}...</p>
-                            <button>Read more</button>
-                        </div>
-                    </a>
-                    @endforeach
-                </div>
-
+                    <button>Read more</button>
+                </a>
             </div>
         </div>
+    </div>
+    @endforeach
+</div>
 @endsection
