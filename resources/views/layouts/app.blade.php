9a3d98ef8d3e0065d033a8c4e7a27d428632d271
OlePundit <glennomondi@yahoo.com>
2024-06-26 04:45:56 +0300

    author

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -38,11 +38,11 @@
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></script>
       <!-- Styles -->
       <style>.ie-panel{display: none;background: #212121;padding: 10px 0;box-shadow: 3px 3px 5px 0 rgba(0,0,0,.3);clear: both;text-align:center;position: relative;z-index: 1;} html.ie-10 .ie-panel, html.lt-ie-10 .ie-panel {display: block;}</style>
-        <!-- Scripts -->
-        <link rel="stylesheet" href="{{ asset('build/assets/app-DJyq2DSN.css') }}">
-        <link rel="stylesheet" href="{{ asset('build/assets/app-sEhFnVTV.css') }}">
-        <link rel="stylesheet" href="{{ asset('build/assets/responsive-8r-qn9KV.css') }}">
-        <script src="{{ asset('build/assets/app-BwDpqcm8.js') }}"></script>
+      <!-- Scripts -->
+      <link rel="stylesheet" href="{{ asset('build/assets/app-B_VZRs9C.css') }}">
+      <link rel="stylesheet" href="{{ asset('build/assets/app-sEhFnVTV.css') }}">
+      <link rel="stylesheet" href="{{ asset('build/assets/responsive-DhAHJOLM.css') }}">
+      <script src="{{ asset('build/assets/app-BwDpqcm8.js') }}"></script>
 
    </head>
    <body class="app">
