32b64589962fe8d71d77628518fdd8a125597b85
OlePundit <glennomondi@yahoo.com>
2024-06-26 05:02:49 +0300

    rounded-circle

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -7,7 +7,7 @@
         <div class="container" style="width:100%;margin:auto;">
             <div class="row section-service" style="align-items:flex-start;justify-content:flex-start;">
                 <div class="col-lg-2 mx-2 col-md-12 d-flex justify-content-center align-items-center author" style="flex-direction:column;margin-top:100px;">
-                    <img src="/storage/images/author.jpg" class="roounded-circle" height="260px" width="260px" alt="author">
+                    <img src="/storage/images/author.jpg" class="rounded-circle" height="260px" width="260px" alt="author">
                     <h5 class="text-center">OlePundit</h5>
                     <h6 class="text-center">{{$slug->created_at}}</h6>
                 </div>
