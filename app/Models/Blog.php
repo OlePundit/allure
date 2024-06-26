2817fa4cbe6b810ede2e20fdfc07d74db0e47392
OlePundit <glennomondi@yahoo.com>
2024-06-26 04:32:35 +0300

    slugifying blogs

--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -7,5 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Blog extends Model
 {
-    use HasFactory;
+    protected $guarded = [];
+
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
+    public function setTitleAttribute($value)
+    {
+        $this->attributes['title'] = $value;
+        $this->attributes['slug'] = Str::slug($value);
+    }
 }
