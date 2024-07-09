2498bb92c14d52effabfd946730d8320b080d015
OlePundit <glennomondi@yahoo.com>
2024-07-09 17:57:37 +0300

    string

--- database/migrations/2024_07_09_144945_add_payment_terms_to_services_table.php
+++ database/migrations/2024_07_09_144945_add_payment_terms_to_services_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('services', function (Blueprint $table) {
-            $table->text('payment_terms');
+            $table->string('payment_terms');
         });
     }
 
