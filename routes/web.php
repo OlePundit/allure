0421686ec897b1597c40e518573ace8fefc42bba
OlePundit <glennomondi@yahoo.com>
2024-07-10 02:08:19 +0300

    booking page

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Route;
 Auth::routes();
 
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::get('/about', [App\Http\Controllers\HomeController::class, 'about'])->name('about');
+Route::get('/book', [App\Http\Controllers\HomeController::class, 'book'])->name('book');
 Route::get('/contact', [App\Http\Controllers\HomeController::class, 'contact'])->name('contact');
 Route::get('/blogs',[App\Http\Controllers\HomeController::class, 'blogs'])->name('blogs');
 Route::get('/blogs/{slug}', [App\Http\Controllers\HomeController::class, 'blog'])->name('blog');
