36e105cc9e42140d741bc5f6028bd2ebb1c69fd0
OlePundit <glennomondi@yahoo.com>
2024-08-15 19:48:34 +0300

    scripts

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -53,8 +53,10 @@
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></script>
         <!-- Styles -->
         <style>.ie-panel{display: none;background: #212121;padding: 10px 0;box-shadow: 3px 3px 5px 0 rgba(0,0,0,.3);clear: both;text-align:center;position: relative;z-index: 1;} html.ie-10 .ie-panel, html.lt-ie-10 .ie-panel {display: block;}</style>
-        @vite(['resources/sass/app.scss', 'resources/js/app.js','resources/css/app.css','resources/css/responsive.css'])
-
+        <link rel="stylesheet" href="{{ asset('build/assets/app-B_VZRs9C.css') }}">
+      <link rel="stylesheet" href="{{ asset('build/assets/app-sEhFnVTV.css') }}">
+      <link rel="stylesheet" href="{{ asset('build/assets/responsive-DhAHJOLM.css') }}">
+      <script src="{{ asset('build/assets/app-BwDpqcm8.js') }}"></script>
 
     </head>
     <body>
