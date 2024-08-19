774d9e3860a4c4726c4bca6cd1007e88b3e9b0ea
OlePundit <glennomondi@yahoo.com>
2024-08-20 00:30:24 +0300

    margin right and left

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -5,7 +5,7 @@
 <div class="blog">
     <div class="row justify-content-center" style="margin-right:0px;margin-left:0px;">
         <div class="container" style="width:100%;margin:auto;">
-            <div class="row section-service" style="align-items:flex-start;justify-content:flex-start;">
+            <div class="row section-service" style="align-items:flex-start;justify-content:flex-start;margin-right:0px;margin-left:0px;">
                 <div class="col-lg-2 mx-2 col-md-12 d-flex justify-content-center align-items-center author" style="flex-direction:column;margin-top:100px;">
                     <img src="/storage/images/author.jpg" class="rounded-circle mb-3" height="260px" width="260px" alt="author">
                     <h5 class="text-center">OlePundit</h5>
