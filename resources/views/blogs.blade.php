8a72766724d41c644758a9c6cc2f74eac61bc085
OlePundit <glennomondi@yahoo.com>
2024-08-19 23:15:31 +0300

    limit title and cards

--- resources/views/blogs.blade.php
+++ resources/views/blogs.blade.php
@@ -9,11 +9,11 @@
         <div class="col-lg-10">
             <div class="row justify-content-center">
             @foreach($blogs as $blog)
-            <div class="col-lg-3 mt-5">
+            <div class="col-lg-3 col-md-4 col-sm-6 col-12 mt-5">
                 <div class="card" style="width: 100%;">
                     <img src="/storage/{{$blog->thumbnail}}" class="card-img-top">
                     <div class="card-body">
-                        <h4 class="card-title">{{$blog->title}}</h4>
+                        <h4 class="card-title">{!! Str::limit(strip_tags($blog->title), 80) !!}</h4>
                         <p class="card-text">{!! Str::limit(strip_tags($blog->body), 120, '...') !!}...</p>
                         <a href="/blogs/{{$blog->slug}}" style="text-decoration:none;" class="custom-card">
 
