a9af69860aabd0ef953e3ea6c6e4c57b58dc510d
OlePundit <glennomondi@yahoo.com>
2024-11-30 19:53:45 +0300

    price

--- database/migrations/2024_11_30_164506_add_price_to_l_p_s_table.php
+++ database/migrations/2024_11_30_164506_add_price_to_l_p_s_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('l_p_s', function (Blueprint $table) {
-            $table->integer('price')
+            $table->integer('price');
         });
     }
 
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('l_p_s', function (Blueprint $table) {
-            $table->dropColumn('price')
+            $table->dropColumn('price');
         });
     }
 };
