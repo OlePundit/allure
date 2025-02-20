f002fbb15ab0eea8ce0e0f65408006caa0ff95a3
OlePundit <glennomondi@yahoo.com>
2025-02-21 02:18:56 +0300

    services

--- resources/views/services.blade.php
+++ resources/views/services.blade.php
@@ -5,61 +5,6 @@
 <div class="services product_section layout_padding">
     <div class="container">
         <div class="service_section_main">
-            <h3>Massage Services</h3>
-            <div class="row">
-                @foreach($massages as $massage)
-
-                <div class="product-wrap col-lg-4 col-md-6 col-12">
-                    <div class="services-thumb">
-                        <img
-                        src="storage/{{$massage->thumbnail}}"
-                        alt="info-img" class="services-image img-fluid" >
-                        <div class="services-info">
-                            <h4>{{$massage->name}}</h4>
-                            <p>{{\Illuminate\Support\Str::limit($massage->description)}} </p>
-                            <div class="d-flex mt-2 duration-wrap">
-                                <span class="card-text"><i class="fa-solid fa-clock"></i>{{$massage->duration}}</span>
-                                <span class=""><i class="fa-solid fa-money-bill"></i>{{$massage->payment_terms}}</span>
-                            </div>
-                            <div class="d-flex price-wrap mt-3">
-                                <p class="card-text">{{$massage->price}}</p>
-                                <a href="/booking/{{$massage->id}}">Book Now</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                @endforeach
-            </div>
-
-
-            <h3>Mani-pedi treat</h3>
-
-            <div class="row product-row">
-                @foreach($manis as $mani)
-
-                <div class="product-wrap col-lg-4 col-md-6 col-12">
-                    <div class="services-thumb">
-                        <img
-                        src="storage/{{$mani->thumbnail}}"
-                        alt="info-img" class="services-image img-fluid" >
-                        <div class="services-info">
-                            <h4>{{$mani->name}}</h4>
-                            <p>{{\Illuminate\Support\Str::limit($mani->description)}} </p>
-                            <div class="d-flex mt-2 duration-wrap">
-                                <span class="card-text"><i class="fa-solid fa-clock"></i>{{$mani->duration}}</span>
-                                <span class=""><i class="fa-solid fa-money-bill"></i>{{$mani->payment_terms}}</span>
-                            </div>
-                            <div class="d-flex price-wrap mt-3">
-                                <p class="card-text">{{$mani->price}}</p>
-                                <a href="/booking/{{$mani->id}}">Book Now</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                @endforeach
-
-
-            </div>
             <h3>Salon</h3>
 
             <div class="row product-row">
@@ -86,7 +31,6 @@
                 </div>
                 @endforeach
             </div>
-
             <h3>Salon(Caucasian Hair)</h3>
 
             <div class="row product-row">
@@ -114,6 +58,188 @@
                 </div>
                 @endforeach
             </div>
+            <h3>Nail Bar</h3>
+
+            <div class="row product-row">
+                @foreach($manis as $mani)
+
+                <div class="product-wrap col-lg-4 col-md-6 col-12">
+                    <div class="services-thumb">
+                        <img
+                        src="storage/{{$mani->thumbnail}}"
+                        alt="info-img" class="services-image img-fluid" >
+                        <div class="services-info">
+                            <h4>{{$mani->name}}</h4>
+                            <p>{{\Illuminate\Support\Str::limit($mani->description)}} </p>
+                            <div class="d-flex mt-2 duration-wrap">
+                                <span class="card-text"><i class="fa-solid fa-clock"></i>{{$mani->duration}}</span>
+                                <span class=""><i class="fa-solid fa-money-bill"></i>{{$mani->payment_terms}}</span>
+                            </div>
+                            <div class="d-flex price-wrap mt-3">
+                                <p class="card-text">{{$mani->price}}</p>
+                                <a href="/booking/{{$mani->id}}">Book Now</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                @endforeach
+
+
+            </div>
+            <h3>Barber Shop</h3>
+            <div class="row">
+                @foreach($barberAs as $barberA)
+
+                <div class="product-wrap col-lg-4 col-md-6 col-12">
+                    <div class="services-thumb">
+                        <img
+                        src="storage/{{$barberA->thumbnail}}"
+                        alt="info-img" class="services-image img-fluid" >
+                        <div class="services-info">
+                            <h4>{{$barberA->name}}</h4>
+                            <p>{{\Illuminate\Support\Str::limit($barberA->description)}} </p>
+                            <div class="d-flex mt-2 duration-wrap">
+                                <span class="card-text"><i class="fa-solid fa-clock"></i>{{$barberA->duration}}</span>
+                                <span class=""><i class="fa-solid fa-money-bill"></i>{{$barberA->payment_terms}}</span>
+                            </div>
+                            <div class="d-flex price-wrap mt-3">
+                                <p class="card-text">{{$barberA->price}}</p>
+                                <a href="/booking/{{$barberA->id}}">Book Now</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                @endforeach
+            </div>
+            <h3>Barber Shop (Caucasian Hair)</h3>
+            <div class="row">
+                @foreach($barberCs as $barberC)
+
+                <div class="product-wrap col-lg-4 col-md-6 col-12">
+                    <div class="services-thumb">
+                        <img
+                        src="storage/{{$barberC->thumbnail}}"
+                        alt="info-img" class="services-image img-fluid" >
+                        <div class="services-info">
+                            <h4>{{$barberC->name}}</h4>
+                            <p>{{\Illuminate\Support\Str::limit($barberC->description)}} </p>
+                            <div class="d-flex mt-2 duration-wrap">
+                                <span class="card-text"><i class="fa-solid fa-clock"></i>{{$barberC->duration}}</span>
+                                <span class=""><i class="fa-solid fa-money-bill"></i>{{$barberC->payment_terms}}</span>
+                            </div>
+                            <div class="d-flex price-wrap mt-3">
+                                <p class="card-text">{{$barberC->price}}</p>
+                                <a href="/booking/{{$barberC->id}}">Book Now</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                @endforeach
+            </div>
+            <h3>Massage Services</h3>
+            <div class="row">
+                @foreach($massages as $massage)
+
+                <div class="product-wrap col-lg-4 col-md-6 col-12">
+                    <div class="services-thumb">
+                        <img
+                        src="storage/{{$massage->thumbnail}}"
+                        alt="info-img" class="services-image img-fluid" >
+                        <div class="services-info">
+                            <h4>{{$massage->name}}</h4>
+                            <p>{{\Illuminate\Support\Str::limit($massage->description)}} </p>
+                            <div class="d-flex mt-2 duration-wrap">
+                                <span class="card-text"><i class="fa-solid fa-clock"></i>{{$massage->duration}}</span>
+                                <span class=""><i class="fa-solid fa-money-bill"></i>{{$massage->payment_terms}}</span>
+                            </div>
+                            <div class="d-flex price-wrap mt-3">
+                                <p class="card-text">{{$massage->price}}</p>
+                                <a href="/booking/{{$massage->id}}">Book Now</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                @endforeach
+            </div>
+            <h3>Body Scrubs</h3>
+            <div class="row">
+                @foreach($bodys as $body)
+
+                <div class="product-wrap col-lg-4 col-md-6 col-12">
+                    <div class="services-thumb">
+                        <img
+                        src="storage/{{$body->thumbnail}}"
+                        alt="info-img" class="services-image img-fluid" >
+                        <div class="services-info">
+                            <h4>{{$body->name}}</h4>
+                            <p>{{\Illuminate\Support\Str::limit($body->description)}} </p>
+                            <div class="d-flex mt-2 duration-wrap">
+                                <span class="card-text"><i class="fa-solid fa-clock"></i>{{$body->duration}}</span>
+                                <span class=""><i class="fa-solid fa-money-bill"></i>{{$body->payment_terms}}</span>
+                            </div>
+                            <div class="d-flex price-wrap mt-3">
+                                <p class="card-text">{{$body->price}}</p>
+                                <a href="/booking/{{$body->id}}">Book Now</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                @endforeach
+            </div>
+            <h3>Facials</h3>
+            <div class="row">
+                @foreach($facials as $facial)
+
+                <div class="product-wrap col-lg-4 col-md-6 col-12">
+                    <div class="services-thumb">
+                        <img
+                        src="storage/{{$facial->thumbnail}}"
+                        alt="info-img" class="services-image img-fluid" >
+                        <div class="services-info">
+                            <h4>{{$facial->name}}</h4>
+                            <p>{{\Illuminate\Support\Str::limit($facial->description)}} </p>
+                            <div class="d-flex mt-2 duration-wrap">
+                                <span class="card-text"><i class="fa-solid fa-clock"></i>{{$facial->duration}}</span>
+                                <span class=""><i class="fa-solid fa-money-bill"></i>{{$facial->payment_terms}}</span>
+                            </div>
+                            <div class="d-flex price-wrap mt-3">
+                                <p class="card-text">{{$facial->price}}</p>
+                                <a href="/booking/{{$facial->id}}">Book Now</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                @endforeach
+            </div>
+            <h3>Waxing</h3>
+            <div class="row">
+                @foreach($waxings as $waxing)
+
+                <div class="product-wrap col-lg-4 col-md-6 col-12">
+                    <div class="services-thumb">
+                        <img
+                        src="storage/{{$waxing->thumbnail}}"
+                        alt="info-img" class="services-image img-fluid" >
+                        <div class="services-info">
+                            <h4>{{$waxing->name}}</h4>
+                            <p>{{\Illuminate\Support\Str::limit($waxing->description)}} </p>
+                            <div class="d-flex mt-2 duration-wrap">
+                                <span class="card-text"><i class="fa-solid fa-clock"></i>{{$waxing->duration}}</span>
+                                <span class=""><i class="fa-solid fa-money-bill"></i>{{$waxing->payment_terms}}</span>
+                            </div>
+                            <div class="d-flex price-wrap mt-3">
+                                <p class="card-text">{{$waxing->price}}</p>
+                                <a href="/booking/{{$waxing->id}}">Book Now</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                @endforeach
+            </div>
+
+
+
+
         </div>
     </div>
 </div>
